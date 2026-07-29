976ea465a304ee4b6432709bb23525d4df58d3ff
lz <zhuang12306@qq.com>
2026-07-29 18:10:21 +0800

    allow frp frontend proxy host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
+    allowedHosts: ['frp-six.com'],
     proxy: {
       '/api': 'http://localhost:8080',
     },
